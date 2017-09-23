9e9654a5a6a16b7e3f825299cbadfb4648328c68
Mobarak Hosen <mobarak.ice.06@gmail.com>
2017-09-23 23:04:25 +0600

    Global, nonlocal, is, in and del keyword test

--- basic/keyword_test.py
+++ basic/keyword_test.py
@@ -111,3 +111,30 @@ else:
 # printing all keywords at once using "kwlist()"
 print ("The list of keywords is : ")
 print (keyword.kwlist)
+
+# Python code to demonstrate
+# True, False, None, and, or , not
+
+# showing that None is not equal to 0
+# prints False as its false.
+print(None == 0)
+
+# showing objective of None
+# two None value equated to None
+# here x and y both are null
+# hence true
+x = None
+y = None
+print(x == y)
+
+# showing logical operation
+# or (returns True)
+print(True or False)
+
+# showing logical operation
+# and (returns False)
+print(False and True)
+
+# showing logical operation
+# not (returns False)
+print(not True)
